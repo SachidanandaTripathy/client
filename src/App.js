3893977b06e8fd8ba5513940fe8b43f3307a3200
SachidanandaTripathy <sachidananda.tripathy03@gmail.com>
2024-03-12 22:36:03 +0530

    personel portofolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+import photo from './Tripathy2.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <div className="container">
+          <nav>
+            <h2 className="logo">Sachidananda..</h2>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About Me</a></li>
+              <li><a href="#">Certifications</a></li>
+              <li><a href="#">Projects</a></li>
+            </ul>
+          </nav>
+          <div className="header-text">
+            <p>Full stack developer</p>
+            <h1>Hey I am <span>Sachidananda Tripathy...</span></h1>
+          </div>
+        </div>
+      </div>
+      <div className="about">
+        <div className="container">
+          <div className="row">
+            <div className="about-col-1">
+              <img src={photo} />
+            </div>
+            <div className="about-col-2">
+              <h1 className='sub-title'>About Me</h1>
+              <p>
+                I am someone who loves to learn new things and Ima very passionate about coding. Ienjoy try ot keep myself
+                updated with hte latest developments ni technology, which inspires me ot constantly improve my skils and learn more.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
